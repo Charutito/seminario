2f9d0fec69951508eb9327d540067b9f36bacdd1
diego_yyz <diego_yyz@hotmail.com>
2018-03-11 19:23:29 -0300

    FAde obstacles added

---
+++ Assets/AssetStore/Standard Assets/Utility/FadeObstacles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeObstacles : MonoBehaviour {
+    public GameObject target;
+    GameObject current;
+    void Update() {
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, transform.forward, Color.green);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Ambient"&& hit.collider.gameObject!= current)
+            {
+                current = hit.collider.gameObject;
+                current.GetComponent<Renderer>().material.SetFloat("_TotalOpacity", 0.5f);
+            }
+            else if (hit.collider.gameObject.tag != "Ambient" && current != null)
+            {
+                current.GetComponent<Renderer>().material.SetFloat("_TotalOpacity", 1f);
+                current = null;
+            }           
+        }
+      
+	}
+}
