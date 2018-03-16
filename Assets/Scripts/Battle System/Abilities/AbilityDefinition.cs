7df1d96c61c2e1d980b7068cb67a4d047e08ee1d
Lucas Fernandez <lfvillafranca@gmail.com>
2018-03-16 01:50:28 -0300

    - Starting Spell system

---
+++ Assets/Scripts/Battle System/Abilities/AbilityDefinition.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BattleSystem.Abilities
+{
+    [CreateAssetMenu(menuName = "Battle System/Abilities/New Ability")]
+    public class AbilityDefinition : ScriptableObject
+    {
+        #region Properties
+        public string Tittle { get { return title; } }
+        public string Tooltip { get { return tooltip; } }
+        public Sprite Sprite { get { return sprite; } }
+        public GameObject Prefab { get { return prefab; } }
+        public AudioClip Sound { get { return sound; } }
+        public float Cooldown { get { return cooldown; } }
+        #endregion
+
+        #region Fields
+        [SerializeField]
+        private string title = "New Ability";
+
+        [SerializeField]
+        private string tooltip = "Beeep BOOOOOOOP!";
+
+        [SerializeField]
+        private Sprite sprite;
+
+        [SerializeField]
+        private GameObject prefab;
+
+        [SerializeField]
+        private AudioClip sound;
+
+        [SerializeField]
+        private float cooldown = 1f;
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Battle System/Abilities/AbilityFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleSystem.Abilities
+{
+    [CreateAssetMenu(menuName = "Battle System/Abilities/New Factory")]
+    public class AbilityFactory : ScriptableObject
+    {
+        public List<AbilityDefinition> Definitions { get { return definitions; } }
+
+        [SerializeField]
+        private List<AbilityDefinition> definitions;
+    }
+}
