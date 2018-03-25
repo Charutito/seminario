d136d4198fd06d3fa16d444c38c573fc28d20bfe
Lucas Fernandez <lfvillafranca@gmail.com>
2018-03-25 16:26:54 -0300

    Character attack speed

--- Assets/Scripts/Entities/CharacterEntity.cs
+++ Assets/Scripts/Entities/CharacterEntity.cs
@@ -2,12 +2,16 @@
 using FSM;
 using Managers;
 using System;
+using UnityEngine;
 
 namespace Entities
 {
     public class CharacterEntity : Entity
     {
+        public float fireRate = 0.5f;
+
         private CharacterFSM fsm;
+        private float nextFire = 0f;
 
         public event Action OnMove = delegate { };
         public event Action OnAttack = delegate { };
@@ -20,8 +24,9 @@ namespace Entities
                 OnMove();
             }
 
-            if (InputManager.Instance.LightAttack && OnAttack != null)
+            if (InputManager.Instance.LightAttack && Time.time > nextFire && OnAttack != null)
             {
+                nextFire = Time.time + fireRate;
                 OnAttack();
             }
 
