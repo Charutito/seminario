f2790485bb385664d2e9a831973c1e3de519ca38
Lucas Fernandez <lfernandez@jamcity.com>
2018-03-28 18:01:17 -0300

    Basic enemy animator FSM - Animation behaviours added

---
+++ Assets/Scripts/Animator Behaviours/SetEntityAnimatorTrigger.cs
@@ -0,0 +1,35 @@
+﻿using Entities;
+using UnityEngine;
+
+namespace Animator_Behaviours
+{
+    public class SetEntityAnimatorTrigger : StateMachineBehaviour
+    {
+        [SerializeField] private string trigger = "";
+        [SerializeField] private bool triggerOnEnter;
+        [SerializeField] private bool triggerOnExit;
+
+        private Entity _entity;
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (_entity == null)
+            {
+                _entity = animator.GetComponentInParent<Entity>();
+            }
+
+            if (triggerOnEnter && _entity != null)
+            {
+                _entity.Animator.ResetTrigger(trigger);
+            }
+        }
+
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (triggerOnExit && _entity != null)
+            {
+                _entity.Animator.ResetTrigger(trigger);
+            }
+        }
+    }
+}
