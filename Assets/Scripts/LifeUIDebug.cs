90c0972639d45650b6ea725a41857b83ef83b5f3
Mafiu <mf.milewski@gmail.com>
2018-04-07 18:24:05 -0300

    [FIX] Controller + Life UI Codigo + 3er Ataque

---
+++ Assets/Scripts/LifeUIDebug.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Entities;
+using Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeUIDebug : MonoBehaviour {
+
+    public Image fill;
+
+    private CharacterEntity _character;
+
+    private void Start()
+    {
+        _character = GameManager.Instance.Character;
+    }
+
+    private void Update()
+    {
+        fill.fillAmount = _character.Stats.Health.Current / _character.Stats.Health.Max;    
+    }
+}
