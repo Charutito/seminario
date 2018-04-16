b60ac85efbb7e20e13afd3f6169692baa69ade6b
diego_yyz <diego_yyz@hotmail.com>
2018-04-15 21:14:02 -0300

    New camera point prefabs

--- Assets/CameraPoint.cs
+++ Assets/CameraPoint.cs
@@ -1,13 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+namespace Managers.Camera
+{
+    public class CameraPoint : MonoBehaviour {
+   
+     BoxCollider TriggerArea;
+     CameraFollow cameraScript;
+     GameObject player;
 
-public class CameraPoint : MonoBehaviour {
-    public float Range;
-	// Use this for initialization
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, Range);
+        // Use this for initialization
+        public void Awake()
+        {
+            TriggerArea = GetComponent<BoxCollider>();
+            cameraScript = GameObject.FindObjectOfType<CameraFollow>();
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                player = cameraScript.target;
+                cameraScript.target = this.gameObject;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            cameraScript.target = player;
+            Debug.Log("hit2");
+
+        }
     }
-}
+    }
+
