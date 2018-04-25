0a1ecd570a0db1d20d2985417189e5e9c8bc3a7b
kurorin321 <31972839+kurorin321@users.noreply.github.com>
2018-04-25 00:30:00 -0300

    cosasRotas

---
+++ Assets/Animations/spiritBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spiritBullet : MonoBehaviour
+
+{
+    Animator _anim;
+	// Use this for initialization
+	void Start () {
+        _anim = GetComponent<Animator>();
+
+        float random = Random.value;
+
+        _anim.SetFloat("randomHit",random);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
