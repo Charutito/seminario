df5881cf1f268646da8b67d3ab37c95f97ff0dd8
diego_yyz <diego_yyz@hotmail.com>
2018-04-25 00:51:22 -0300

    Spirit bullet fix

--- Assets/Animations/spiritBullet.cs
+++ Assets/Animations/spiritBullet.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class spiritBullet : MonoBehaviour
 
 {
     Animator _anim;
+
+
 	// Use this for initialization
 	void Start () {
-        _anim = GetComponent<Animator>();
-
-        float random = Random.value;
-
-        _anim.SetFloat("randomHit",random);
+      
+        
+        _anim.SetInteger("RandomAttack", Random.Range(0,2));
 	}
 	
 	// Update is called once per frame
