30a78d42d2980e0ae263048b101285d052610a87
diego_yyz <diego_yyz@hotmail.com>
2018-05-01 19:32:50 -0300

    graviton fix

--- Assets/Scripts/Battle System/Spells/GravitonCaster.cs
+++ Assets/Scripts/Battle System/Spells/GravitonCaster.cs
@@ -6,23 +6,16 @@ namespace BattleSystem.Spells
 {
 	public class GravitonCaster : MonoBehaviour
 	{
-		public GameObject PositiveGraviton;
 		public GameObject NegativeGraviton;
         public bool type;
         public void cast()
 		{
 			if (!type)
 			{
-                var spell = Instantiate(NegativeGraviton, transform);
-                Destroy(spell.gameObject,3);
+                var spell = Instantiate(NegativeGraviton);
+                spell.transform.position = transform.position;
                 Destroy(gameObject);
-			}
-			else if (type)
-            {
-                var spell = Instantiate(PositiveGraviton, transform);
-                Destroy(spell.gameObject, 3);
-                Destroy(gameObject);
-			}
-		}
+			}			
+        }
 	}
 }
