52480bf6d12e8e6c4660cc138994f1f7f7541c00
Mafiu <mf.milewski@gmail.com>
2018-05-02 01:00:47 -0300

    [ABILITY] Wolf effect

---
+++ Assets/WolfEffect/WolfEffect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfEffect : MonoBehaviour
+{
+    public Renderer rend;
+    public Material newMaterialRef;
+    public Shader dissolve;
+    private bool activeEffect;
+    private float shininess = 0.5f;
+
+    void Start()
+    {
+        if(rend != null)
+            rend = GetComponent<Renderer>();
+
+        rend.material = newMaterialRef;
+        rend.material.shader = dissolve;
+        //rend.material.shader = Shader.Find("_Cutout");
+    }
+
+    private void Update()
+    {
+        shininess -= 0.7f * Time.deltaTime;
+        if(shininess >= 0.01f)
+        {
+            rend.material.SetFloat("_Cutout", shininess);
+        }  
+    }
+
+    public void BackToStart()
+    {
+        shininess = 0.5f;
+    }
+}
