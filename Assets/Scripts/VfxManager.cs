cb85161e51e9bfc44b89388a05111ac1b7c9af4f
Diegoyyz <diego_yyz@hotmail.com>
2018-05-17 00:32:13 -0300

    Particulas de dash y wolf punch

---
+++ Assets/Scripts/VfxManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Entities
+{
+    public class VfxManager : MonoBehaviour
+    {
+        public CharacterEntity Character;
+        public GameObject dashPArts;
+        // Use this for initialization
+        void Start()
+        {
+            Character.OnDash += Dash;
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void OnDestroy()
+        {
+
+        }
+        private void Dash()
+        {
+            dashPArts.SetActive(true);
+            StartCoroutine("DeactivatePArts");
+        }
+         IEnumerator DeactivatePArts()
+        {
+            yield return new WaitForSeconds(0.5f);
+            dashPArts.SetActive(false);
+
+
+        }
+
+    }
+}
+
