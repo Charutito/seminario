8d76ce4383ac2ba46b75e30f49cb063041a06424
Mafiu <mf.milewski@gmail.com>
2018-05-21 00:10:03 -0300

    [FIX] Graviton time, Wolf Punch , Drone prefab

---
+++ Assets/WolPunch/SpawnWolfEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWolfEffect : MonoBehaviour {
+
+    public GameObject wolf;
+
+	public void SpawnWolf()
+    {
+        Instantiate(wolf,this.transform.position, transform.rotation);        
+    }
+
+}
---
+++ Assets/WolPunch/WolfMoveAndDestroy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfMoveAndDestroy : MonoBehaviour {
+
+	void Update () {
+        this.transform.position += transform.forward * Time.deltaTime * 7;
+        Destroy(this.gameObject, 0.5f);
+	}
+}
