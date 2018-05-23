28c101dcb3136a9419a28fe46e24d752a40f2c7c
Lucas Fernandez <lfvillafranca@gmail.com>
2018-05-22 22:40:12 -0300

    Cinemachine testing and bug fixing #159 | Parry de las balas #162 | Vida y mana con F1 y F2

--- Assets/Scripts/BulletMove.cs
+++ Assets/Scripts/BulletMove.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Managers;
+using Metadata;
 using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
     public float Speed = 10;
+    public float SpeedBoostMultiplier = 1.5f;
     private int _direction = 1;
-		
+
     private void Update()
     {
         transform.localPosition += transform.forward * _direction * Speed * Time.deltaTime;
@@ -14,6 +17,8 @@ public class BulletMove : MonoBehaviour
 
     public void ChangeDir()
     {
+        gameObject.layer = GameManager.Instance.Character.gameObject.layer;
+        Speed *= SpeedBoostMultiplier;
         _direction = -_direction;
     }
 }
--- Assets/Scripts/Utility/DebugShortcuts.cs
+++ Assets/Scripts/Utility/DebugShortcuts.cs
@@ -1,4 +1,6 @@
-﻿#if UNITY_EDITOR
+﻿
+using Managers;
+#if UNITY_EDITOR
 using UnityEditor;
 #endif
 using UnityEngine;
@@ -8,16 +10,46 @@ namespace Utility
 {
     public class DebugShortcuts : MonoBehaviour
     {
-        [SerializeField] private KeyCode editorPauseKey = KeyCode.F12;
+        [SerializeField] private KeyCode _editorPauseKey = KeyCode.F12;
+        [SerializeField] private KeyCode _recoverHealthCharacter = KeyCode.F1;
+        [SerializeField] private KeyCode _recoverSpiritCharacter = KeyCode.F2;
+
+
+        public void RecoverHealthCharacter()
+        {
+            var character = GameManager.Instance.Character;
+            character.Heal(character.Stats.Health.Max);
+        }
+        
+        public void RecoverSpiritCharacter()
+        {
+            var character = GameManager.Instance.Character;
+            character.HealEnergy(character.Stats.Spirit.Max);
+        }
+        
+        public void ToggleCharacterNoClip()
+        {
+            var character = GameManager.Instance.Character;
+            character.Agent.enabled = !character.Agent.enabled;
+        }
 
         private void Update()
         {
 #if UNITY_EDITOR
-            if (Input.GetKeyDown(editorPauseKey))
+            if (Input.GetKeyDown(_editorPauseKey))
             {
                 EditorApplication.isPaused = true;
             }
 #endif
+            if (Input.GetKeyDown(_recoverHealthCharacter))
+            {
+                RecoverHealthCharacter();
+            }
+            
+            if (Input.GetKeyDown(_recoverSpiritCharacter))
+            {
+                RecoverSpiritCharacter();
+            }
         }
     }
 }
