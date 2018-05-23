5e4af65cb6821b702976450da27f822401cefdaf
Lucas Fernandez <lfvillafranca@gmail.com>
2018-05-22 23:44:33 -0300

    Added dolly cinemachine tracks Order in Scene elements

---
+++ Assets/Scripts/Utility/TriggerCameraCinemachine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+namespace Utility
+{
+    public class TriggerCameraCinemachine : MonoBehaviour
+    {
+        public CinemachineVirtualCamera Camera;
+
+        private void ToggleCamera(bool state)
+        {
+            Camera.gameObject.SetActive(state);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            ToggleCamera(true);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            ToggleCamera(false);
+        }
+    }
+}
