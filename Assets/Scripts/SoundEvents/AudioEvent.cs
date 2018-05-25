150d235efabc6f25f48db0681ff41a3b4adc4fb5
Lucas Fernandez <lfvillafranca@gmail.com>
2018-05-25 18:27:36 -0300

    [SFX] Shield generator destroyed [SFX] Shield generator cast shield

--- Assets/Scripts/SoundEvents/AudioEvent.cs
+++ Assets/Scripts/SoundEvents/AudioEvent.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public abstract class AudioEvent : ScriptableObject
 {
 	public abstract void Play(AudioSource source);
+	public abstract void Play(Vector3 position);
 
 	public void Play()
 	{
