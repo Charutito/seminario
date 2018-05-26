08feac9cbc3e45071535830c9ba67e620f6e15d3
Lucas Fernandez <lfvillafranca@gmail.com>
2018-05-26 17:21:47 -0300

    [SFX] Added shield generator hit SFX Reorder

--- Assets/Scripts/Battle System/ShieldGeneratorEntity.cs
+++ Assets/Scripts/Battle System/ShieldGeneratorEntity.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using BattleSystem;
 using Entities;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Entities
 {
@@ -18,38 +20,6 @@ namespace Entities
         private float _currentTimeToCast;
         private List<EnemyShield> _activeShields;
         
-        private void SetupShield()
-        {
-            var entity = CurrentZone.entities
-                                        .Where(e => e != this)
-                                        .Where(e => _activeShields.All(activeEntity => activeEntity.Entity != e))
-                                        .OrderBy(e => Random.value)
-                                        .FirstOrDefault();
-
-            if (entity != null)
-            {
-                var newObject = Instantiate(ShieldPrefab, entity.transform.position, Quaternion.identity, entity.transform);
-                
-                _activeShields.Add(newObject.GetComponent<EnemyShield>());
-                EntitySounds.PlayEffect("Cast", entity.transform.position);
-            }
-
-            _currentTimeToCast = CastDelay.GetRandom;
-        }
-
-        private void HandleDeath(Entity obj)
-        {
-            EntitySounds.PlayEffect("Destroy");
-            _active = false;
-            foreach (var activeShield in _activeShields)
-            {
-                if (activeShield != null)
-                {
-                    Destroy(activeShield.gameObject);
-                }
-            }
-            Destroy(gameObject, 1f);
-        }
 
         protected override void Update()
         {
@@ -70,7 +40,47 @@ namespace Entities
         {
             _activeShields = new List<EnemyShield>();
             OnDeath += HandleDeath;
+            OnTakeDamage += OnOnTakeDamage;
             OnSetAction += (action, groupAction) => _active = true;
+        }
+        
+        private void SetupShield()
+        {
+            var entity = CurrentZone.entities
+                .Where(e => e != this)
+                .Where(e => _activeShields.All(activeEntity => activeEntity.Entity != e))
+                .OrderBy(e => Random.value)
+                .FirstOrDefault();
+
+            if (entity != null)
+            {
+                var newObject = Instantiate(ShieldPrefab, entity.transform.position, Quaternion.identity, entity.transform);
+                
+                _activeShields.Add(newObject.GetComponent<EnemyShield>());
+                EntitySounds.PlayEffect("Cast", entity.transform.position);
+            }
+
+            _currentTimeToCast = CastDelay.GetRandom;
+        }
+        
+        private void HandleDeath(Entity obj)
+        {
+            EntitySounds.PlayEffect("Destroy");
+            _active = false;
+            
+            foreach (var activeShield in _activeShields)
+            {
+                if (activeShield != null)
+                {
+                    Destroy(activeShield.gameObject);
+                }
+            }
+            Destroy(gameObject, 1f);
+        }
+
+        private void OnOnTakeDamage(Damage damage)
+        {
+            EntitySounds.PlayEffect("Hit", transform.position);
         }
     }
 }
