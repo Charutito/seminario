87e45a41f2c27dab39cac97d27ec301f52de1f16
Diegoyyz <diego_yyz@hotmail.com>
2018-05-30 19:50:39 -0300

    loot box y vending machines destructible

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -5,14 +5,42 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour, IDamageable
 {
-    public GameObject drop;
+    public GameObject[] drop;
+       
     public GameObject particles;
+    
+    public int maxHits;
+    int currentHits=0;
     public AudioEvent DestroyAudio;
     public void TakeDamage(Damage damage){
-        Instantiate(drop, transform.position, Quaternion.identity);
-        DestroyAudio.PlayAtPoint(transform.position);
-        var parts = Instantiate(particles, transform.position, Quaternion.identity);
-        Destroy(parts, 1);
-        Destroy(this.gameObject);
+        if (currentHits==maxHits)
+        {
+            foreach (var item in drop)
+            {
+                Instantiate(item, transform.position, Quaternion.identity);
+            }
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            StartCoroutine("FlashCorroutine");
+            DestroyAudio.PlayAtPoint(transform.position);
+            var parts = Instantiate(particles, transform.position, transform.rotation);
+            Destroy(parts, 1);
+            currentHits++;
+        }
+        
+    }
+    IEnumerator FlashCorroutine()
+    {
+        Debug.Log("hit");
+        for (int i = 0; i < 2; i++)
+        {
+
+            gameObject.GetComponentInChildren<Renderer>().material.SetColor("_Color", Color.red);
+            yield return new WaitForSeconds(0.01f);
+            gameObject.GetComponentInChildren<Renderer>().material.SetColor("_Color", Color.white);
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 }
