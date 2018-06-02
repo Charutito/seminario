e2ff10d83c1221f0e31db68989f76b56c4b4d1bf
Lucas Fernandez <lfvillafranca@gmail.com>
2018-06-02 19:27:53 -0300

    Bullet bounce behaviour

---
+++ Assets/Scripts/BulletBounceBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using Managers;
+using UnityEngine;
+
+public class BulletBounceBehaviour : BulletMove
+{
+    public override void TakeDamage()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("BulletBounce"))
+        {
+            Direction = Vector3.Reflect(Direction, -other.transform.right);
+        }
+    }
+}
