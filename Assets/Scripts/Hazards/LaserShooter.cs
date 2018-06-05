fc9f28ccc08fa1a6097dff101eecd7d39c7baaca
Diegoyyz <diego_yyz@hotmail.com>
2018-06-05 11:38:14 -0300

    LaserShooter y laserCross, hazard test scene para probar hazards

---
+++ Assets/Scripts/Hazards/LaserShooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserShooter : MonoBehaviour {
+    public float FireRate;
+    float nextfire;
+    public GameObject Prefab;
+    public Transform canon;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time > nextfire)
+        {
+            Fire();
+            nextfire = Time.time + FireRate;
+        }
+    }
+    void Fire()
+    {
+        var bullet = Instantiate(Prefab);
+        bullet.transform.position = canon.transform.position ;
+        bullet.transform.rotation = canon.transform.rotation;
+    }
+}
