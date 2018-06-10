2cf52a2ccb9eaef6513c11d09c2cbd3f837e0b6a
Diegoyyz <diego_yyz@hotmail.com>
2018-06-10 03:25:26 -0300

    LaserTimer Hazard

---
+++ Assets/Scripts/Hazards/TimerLaser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerLaser : MonoBehaviour {
+    public float FireRate;
+    public float PauseRate;
+
+    float NextOn;
+    float nextOf;
+    bool firing;
+    public GameObject Laser;
+
+
+
+    void Update()
+    {
+
+        if (Time.time > NextOn)
+        {
+            Fire();
+            NextOn = Time.time + FireRate;
+        }
+       
+    }
+    void Fire()
+    {
+        if (firing)
+        {
+            Laser.SetActive(false);
+            firing = false;
+        }
+        else
+        {
+            Laser.SetActive(true);
+            firing = true;
+        }
+    }
+
+}
