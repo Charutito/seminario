cafb69f93ff17ab623cf6a90bd47602de255622c
Lucas Fernandez <lfvillafranca@gmail.com>
2018-06-10 20:49:06 -0300

    Fixed teleport last position

--- Assets/Scripts/Environment/GameLevelTeleporter.cs
+++ Assets/Scripts/Environment/GameLevelTeleporter.cs
@@ -1,17 +1,29 @@
 ﻿using Managers;
+using SaveSystem;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 using UnityEngine;
+using Util;
 
 namespace Environment
 {
     public class GameLevelTeleporter : MonoBehaviour
     {
         public string SceneName;
+        
+        [SerializeField]
+        private bool _active = true;
+
+        public bool IsActive {
+            get { return _active; }
+            set { _active = value; }
+        }
 
         private void OnTriggerEnter(Collider other)
         {
+            if (!_active) return;
+            
             GameManager.Instance.LoadScene(SceneName);
         }
     }
