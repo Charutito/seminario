30235e37229fc8ff6ea151e1bbde82ed5bf497d9
Lucas Fernandez <lfvillafranca@gmail.com>
2018-06-20 19:00:32 -0300

    Random animator parameter

---
+++ Assets/Scripts/Utility/Animator/AnimatorRandomParamater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility
+{
+    public class AnimatorRandomParamater : StateMachineBehaviour
+    {
+        [MinMaxRange(0, 10)]
+        public RangedFloat Range;
+        
+        public string TriggerName = string.Empty;
+        public bool IsFloat = false;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+        {
+            if (TriggerName == string.Empty) return;
+            
+            if (IsFloat)
+            {
+                animator.SetFloat(TriggerName, Range.GetRandom);
+            }
+            else
+            {
+                animator.SetInteger(TriggerName, Range.GetRandomInt);
+            }
+        }
+    }
+}
