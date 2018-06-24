1d7a1846a72fff6d212a85acf54844e4d7c56fcb
Diegoyyz <diego_yyz@hotmail.com>
2018-06-24 01:53:06 -0300

    Nueva escena y vehiculos policia

---
+++ Assets/Scripts/Siren.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Siren : MonoBehaviour {
+    public Renderer blue;
+    public float blueLevel;
+    public Renderer red;
+    public float redLevel;
+
+    public void Start()
+    {
+        blue.material.SetFloat("_fresnelbiasss", blueLevel);
+        red.material.SetFloat("_fresnelbiasss", redLevel);
+    }
+    private void Update()
+    {
+        blue.material.SetFloat("_fresnelbiasss", blueLevel);
+        red.material.SetFloat("_fresnelbiasss", redLevel);
+
+    }
+}
