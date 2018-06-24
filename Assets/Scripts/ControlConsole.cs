d8b0c25a4b51fdcf1d3f2a865a006265bb4942d9
Diegoyyz <diego_yyz@hotmail.com>
2018-06-24 15:36:58 -0300

    door wit control consoles

---
+++ Assets/Scripts/ControlConsole.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ControlConsole : MonoBehaviour {
+    public event Action OnConsoleDestroy = ()=>{ };
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void OnDestroy()
+    {
+        OnConsoleDestroy();
+    }
+}
---
+++ Assets/Scripts/EndDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDoor : MonoBehaviour {
+    public ControlConsole[] consoles;
+    public GameObject[] lasers;
+    int lifes;
+
+	void Awake () {
+        lifes = lasers.Length-1;
+        foreach (var item in consoles)
+        {
+            item.OnConsoleDestroy += LifeLoss;
+        }
+	}
+    void LifeLoss()
+    {
+        if (lasers[lifes]!=null)
+        {
+            Destroy(lasers[lifes]);
+        }
+        lifes--;
+
+    }
+}
