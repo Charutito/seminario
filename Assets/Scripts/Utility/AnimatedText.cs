afedcba81fe76b9123bc597aa7df09ea59664fc4
Mafiu <mf.milewski@gmail.com>
2018-07-03 02:39:19 -0300

    [CREDITS] Credits screen added

---
+++ Assets/Scripts/Utility/AnimatedText.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AnimatedText : MonoBehaviour
+{
+    
+    public float letterPaused = 0.01f;
+    
+    public string message;
+    
+    public Text textComp;
+
+    public float timeToCleanMessage = 0.0f;
+
+    
+    void Start()
+    {
+        
+        textComp = GetComponent<Text>();
+        
+        message = textComp.text;
+        
+        textComp.text = "";
+
+        StartCoroutine(TypeText());
+        StartCoroutine(CleanMessage());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in message.ToCharArray())
+        {
+            textComp.text += letter;
+            yield return 0;
+            yield return new WaitForSeconds(letterPaused);
+        }
+    }
+
+    IEnumerator CleanMessage()
+    {
+        yield return new WaitForSeconds(timeToCleanMessage);
+        textComp.text = "";
+    }
+}
