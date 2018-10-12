9e36ae6ba99ff5b983a22208855c10dc648db855
Lucas Fernandez <lfvillafranca@gmail.com>
2018-10-12 20:58:40 -0300

    Testing automated builder

---
+++ Assets/Scripts/Builder/Editor/BuilderScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+
+public class BuilderScript
+{
+    private const string BUILD_PATH = "Builds/{0}/{1}/";
+    
+    private static string BuildPathRoot
+    {
+        get
+        {
+            string path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop), ProductName);
+            if (!System.IO.Directory.Exists(path))
+                System.IO.Directory.CreateDirectory(path);
+            return path;
+        }
+    }
+    
+    public static void BuildWindows64()
+    {
+        GenericBuild(BuildTarget.StandaloneWindows64);
+    }
+
+    private static BuildPlayerOptions GetDefaultOptions()
+    {
+        return new BuildPlayerOptions
+        {
+            scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray(),
+            options = BuildOptions.None
+        };
+    }
+
+    private static void GenericBuild(BuildTarget target)
+    {
+        string pathToDeploy = string.Format(BUILD_PATH, Application.version, target);
+        
+        BuildPlayerOptions buildPlayerOptions = GetDefaultOptions();
+        buildPlayerOptions.target = target;
+        buildPlayerOptions.locationPathName = pathToDeploy;
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
