ad10842650a344441de47e708d0e2b35c5b5e12a
Lucas Fernandez <lfvillafranca@gmail.com>
2018-10-12 23:37:40 -0300

    Fixed builder script

--- Assets/Scripts/Builder/BuilderScript.cs
+++ Assets/Scripts/Builder/BuilderScript.cs
@@ -1,13 +1,34 @@
-﻿using System.Linq;
-using UnityEngine;
+﻿using System.IO;
+using System.Linq;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class BuilderScript
 {
-    private const string BUILD_PATH = "Builds/{0}/{1}/";
+    private const string BUILD_PATH = "{0}_{1}H{2}M_{3}";
+    private const string DEFAULT_EXTENSION = ".exe";
     
+    private static string BuildPathRoot
+    {
+        get
+        {
+            string path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop), "Builds");
+            if (!System.IO.Directory.Exists(path))
+                System.IO.Directory.CreateDirectory(path);
+            return path;
+        }
+    }
+    
+    public static string ProductName
+    {
+        get
+        {
+            return PlayerSettings.productName;
+        }
+    }
+    
+    [MenuItem("Test/Menu")]
     public static void BuildWindows64()
     {
         GenericBuild(BuildTarget.StandaloneWindows64);
@@ -24,11 +45,12 @@ public class BuilderScript
 
     private static void GenericBuild(BuildTarget target)
     {
-        string pathToDeploy = string.Format(BUILD_PATH, Application.version, target);
+        string path = Path.Combine(Path.Combine(BuildPathRoot, target.ToString()), string.Format(BUILD_PATH, ProductName, System.DateTime.Now.ToString("dd.MM.yyyy_HH"), System.DateTime.Now.ToString("mm"), target));
+        string name = ProductName + DEFAULT_EXTENSION;
         
         BuildPlayerOptions buildPlayerOptions = GetDefaultOptions();
         buildPlayerOptions.target = target;
-        buildPlayerOptions.locationPathName = pathToDeploy;
+        buildPlayerOptions.locationPathName = Path.Combine(path, name);
 
         BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
